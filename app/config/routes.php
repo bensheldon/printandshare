3af09eba2b89bdb70fcc779c1b8add042141b08a
Ben Sheldon <bensheldon@gmail.com>
2013-08-01 16:23:26 -0700

    store configuration info for easier development setup

---
+++ app/config/routes.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Routes configuration
+ *
+ * In this file, you set up routes to your controllers and their actions.
+ * Routes are very important mechanism that allows you to freely connect
+ * different urls to chosen controllers and their actions (functions).
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.app.config
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
+Router::connect('/', array('controller' => 'proposals', 'action'=>'index'));
+
